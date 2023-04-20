e7552f2573ec37dae3c302ebc42680db5fa54d31
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2023-04-21 00:26:08 +0400

    Update router.php

--- controller/router.php
+++ controller/router.php
@@ -3,10 +3,9 @@
 include 'Calculator.php';
 
 if(isset($_POST["submit"])){
-    
     $num1 = $_POST["num1"]; 
     $num2 = $_POST["num2"]; 
     $operator = $_POST["operator"];
-    $calculation = new Calculator($num1, $num2, $operator);
-    echo $calculation->getResult();
+    $calculator = new Calculator($num1, $num2, $operator);
+    echo $calculator->getResult();
 }
